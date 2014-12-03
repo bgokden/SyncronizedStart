1d5d15dcdf6c63aed2609cbc5b141f7e70e8cc21
Berk Gokden <berkgokden@gmail.com>
2014-12-04 00:23:15 +0200

    Syncronization and test

--- src/main/java/com/berkgokden/App.java
+++ src/main/java/com/berkgokden/App.java
@@ -1,13 +1,77 @@
 package com.berkgokden;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.NetworkConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ICountDownLatch;
+import com.hazelcast.core.IMap;
+import org.apache.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final Logger logger = Logger.getLogger(App.class);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Prepare Hazelcast cluster
+        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+        ICountDownLatch latch = hazelcastInstance.getCountDownLatch( "countDownLatch" );
+
+        boolean success = printWeAreStartedWhenReady(hazelcastInstance, 10);
+
+        System.out.println( "Completed");
+        hazelcastInstance.shutdown();
+
     }
+
+    public static boolean printWeAreStartedWhenReady(HazelcastInstance hazelcastInstance, int numberOfNodes)
+    {
+
+        ICountDownLatch latch = hazelcastInstance.getCountDownLatch( "countDownLatch" );
+        latch.trySetCount( numberOfNodes );
+
+        latch.countDown();
+
+        boolean result = false;
+        try {
+            boolean success = latch.await( 10, TimeUnit.MINUTES );
+            if (success) {
+                IMap<String, Boolean> map = hazelcastInstance.getMap("debugging");
+
+                Boolean isWeAreStartedPrinted = map.get("is-we-are-started-printed");
+
+                if (isWeAreStartedPrinted == null) {
+                    map.putIfAbsent("is-we-are-started-printed", false);
+                    if (map.tryLock("is-we-are-started-printed")) {
+                        isWeAreStartedPrinted = map.get("is-we-are-started-printed");
+                        if (!isWeAreStartedPrinted) {
+                            System.out.println("We are started!");
+                            map.put("is-we-are-started-printed", true);
+                            result = true;
+                        }
+                        map.unlock("is-we-are-started-printed");
+                    }
+                }
+            } else {
+                System.err.println("Something may have gone wrong. We are not fully started!");
+            }
+        } catch (InterruptedException e) {
+            System.err.println("Something may have gone wrong. We are not fully started!");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Something may have gone wrong. We are not fully started!");
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
 }
--- src/test/java/com/berkgokden/AppTest.java
+++ src/test/java/com/berkgokden/AppTest.java
@@ -1,38 +1,78 @@
 package com.berkgokden;
 
-import junit.framework.Test;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.HazelcastInstanceAware;
+import com.hazelcast.core.ICountDownLatch;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+    private static final Logger logger = Logger.getLogger(AppTest.class);
+    @Test
+    public void shouldReturnTrueWhenWeAreStartedPrintedOnlyOnce() {
+        final int NUMBEROFINSTANCES = 10;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+        ExecutorService executorService = Executors.newFixedThreadPool(NUMBEROFINSTANCES);
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+        List<Callable<Boolean>> callables = new ArrayList<Callable<Boolean>>(NUMBEROFINSTANCES);
+        for (int i = 0; i < NUMBEROFINSTANCES; i++) {
+            callables.add(new Callable<Boolean>() {
+                @Override
+                public Boolean call() throws Exception {
+                    // Prepare Hazelcast cluster
+                    Config config = new Config() ;
+                    config.setProperty( "hazelcast.system.log.enabled", "false" );
+                    HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance(config);
+                    ICountDownLatch latch = hazelcastInstance.getCountDownLatch("countDownLatch");
+
+                    boolean success = App.printWeAreStartedWhenReady(hazelcastInstance, NUMBEROFINSTANCES);
+
+                    System.out.println("Completed");
+                    hazelcastInstance.shutdown();
+
+                    return success;
+                }
+
+            });
+        }
+
+        int numberOfSuccesfulPrints = 0;
+        try {
+            List<Future<Boolean>> resultList = executorService.invokeAll(callables, 10, TimeUnit.MINUTES);
+
+            for (int i = 0; i < NUMBEROFINSTANCES; i++) {
+                if (resultList.get(i).get().booleanValue()) {
+                    numberOfSuccesfulPrints++;
+                }
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            numberOfSuccesfulPrints = -1;
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            numberOfSuccesfulPrints = -1;
+        }
+
+        executorService.shutdown();
+
+        logger.debug("Number Of Successful :" + numberOfSuccesfulPrints);
+        Assert.assertTrue("Only one print event should occur.", numberOfSuccesfulPrints == 1);
+
+        }
     }
-}
